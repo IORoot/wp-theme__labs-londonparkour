ecf57fd1e4d93deb5111984f4e866c5ff339633d
Andy Pearson <andy@londonparkour.com>
2021-06-20 08:01:12 +0100

    New LAB Logo

--- header.php
+++ header.php
@@ -18,6 +18,7 @@
 			wp_head(); 
 			do_action('page_builder_header_code'); 
 		?>
+		<?php include( __DIR__.'/src/components/favicons/favicons.php' );  ?>
 	</head>
 
 
--- src/components/main-menu-logo/main-menu-logo.php
+++ src/components/main-menu-logo/main-menu-logo.php
@@ -1,3 +1,9 @@
-<a class="logo flex-1" href="/" >
-    <svg class="h-16 w-32 sm:w-48 fill-white"><use xlink:href="#h-logo-text" /></svg>
-</a>
+<h1 class="flex-1">
+    <a class="logo" href="/" title="London Parkour LAB">
+        <svg role="img" aria-label="London Parkour LAB Logo" class="h-16 w-32 sm:w-48 fill-white hover:fill-gray-900">
+            <title>London Parkour LAB</title>
+            <desc>Company Logo</desc>
+            <use xlink:href="#h-logo-text" />
+        </svg>
+    </a>
+</h1>
