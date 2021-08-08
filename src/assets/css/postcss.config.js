5e2453edd6cc19e5fbf32abdcc56219e4dfbf021
Andy Pearson <andy@londonparkour.com>
2021-08-08 10:25:57 +0100

    postcss-import instead of postcss-partial-import

--- src/assets/css/postcss.config.js
+++ src/assets/css/postcss.config.js
@@ -3,7 +3,7 @@ const tailwindcss = require("tailwindcss");
 
 module.exports = {
     plugins: [
-        require('postcss-partial-import'),
+        require('postcss-import'),
         tailwindcss("tailwind.config.js"),
         require('postcss-nested'),
         require('autoprefixer'),
